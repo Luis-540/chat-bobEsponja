c745bc1e55b17ec8bc03657f5c838de46d063dda
Luis-540 <80134867+Luis-540@users.noreply.github.com>
2021-11-08 18:22:56 -0600

    Update sw-auxiliar.js

--- js/sw-auxiliar.js
+++ js/sw-auxiliar.js
@@ -1,17 +1,14 @@
 
-function actualizarCacheDinamico(dynamicCache, req, res){
-
-  if(res.ok){
-    return caches.open(dynamicCache).then(cache=>{
-      cache.put(req,res.clone());
-      return res.clone();
-    });
-  }
-  else{
-    return res;
-
-
-  }
-
-
+function actualizarCacheDinamico(dynamicCache,req,res){
+    if(res.ok){
+        return caches.open(dynamicCache).then( cache => {
+            cache.put(req,res.clone());
+            return res.clone();
+        });
+    }else{
+        // return fetch(event.request).then(new_response => {
+        //     actualizarCacheDinamico(DYNAMIC_CACHE,event.request,new_response);
+        // });
+        return res;
+    }
 }
